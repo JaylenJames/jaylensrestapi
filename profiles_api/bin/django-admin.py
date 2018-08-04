6a918751db9a887296d20e4a4a40d9e2c0b043ec
JaylenJames <JaylenJames@Jaylens-MacBook-Air.local>
2018-08-04 13:55:20 -0500

    please work adding project files first time

---
+++ profiles_api/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/JaylenJames/OneDrive/U/Udemy/workspace/profiles_rest_api_two/profiles_api/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
